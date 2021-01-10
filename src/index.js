72663560f688561cb664791d2ad74becc78d2951
Sherrpass <sherwinpoh3@gmail.com>
2021-01-10 21:43:26 +0800

    before deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Dashboard from "./Dashboard";
-import Landing from "./Landing";
-import Register from "./Register";
-import Login from "./Login";
 import "./App.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
